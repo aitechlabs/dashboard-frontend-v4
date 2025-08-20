3f76afb2d8f43c0d9a3e97bb3aed2ad2b05b35c7
aitechlabs <133147451+aitechlabs@users.noreply.github.com>
2025-08-20 09:33:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkbg: "#2b2b2b",
+        darkgray: "#1e1e1e"
+      }
+    },
+  },
+  plugins: [],
+}
